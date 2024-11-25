4d6f2957199cfea03714a5be5227a0037553e5a4
Osori <kunshim@naver.com>
2024-11-26 00:02:29 +0900

    add some scripts

---
+++ Assets/Scripts/OneM2M.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class OneM2M
+{
+    public static void sendContentInstance(string containerName, float value)
+    {
+
+    }
+    public static void createContainer(string containerName)
+    {
+
+    }
+}
